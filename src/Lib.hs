8828c21c91338f48dda315a84b012b4ba10f9bd1
Johannes Waldmann <@>
2016-09-15 12:29:45 +0200

    include more libs, make shorter main program

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,16 +6,21 @@
 {-# language FlexibleContexts #-}
 {-# language DeriveGeneric #-}
 {-# language ScopedTypeVariables #-}
+{-# language DataKinds #-}
+{-# language TypeFamilies #-}
+{-# language AllowAmbiguousTypes #-}
 
 module Lib where
 
 -- for outputs of our program:
-import Text.PrettyPrint.HughesPJ 
+import qualified Text.PrettyPrint.HughesPJ as Doc
 
 -- for measuring their performance:
 import qualified Text.PrettyPrint.HughesPJ as TPH 
 import qualified Text.PrettyPrint.Leijen as TPL
 import qualified Text.PrettyPrint.Free as TPF
+import qualified Text.PrettyPrint.ANSI.Leijen as TPAL
+import qualified Text.PrettyPrint.Mainland as TPM
 
 import Series
 import GHC.Generics
@@ -24,17 +29,28 @@ import Control.Monad ( forM, forM_, guard, replicateM )
 import Data.List ( transpose, intersperse )
 import Data.Char (toLower)
 
-import Data.Proxy
+data Proxy (t :: Tag) = Proxy
+
+data Tag = TPH | TPL | TPF | TPAL | TPM
+  deriving (Eq, Ord, Show)
+
+class Printer ( p :: Tag ) where type Doc p
+
+instance Printer TPH where type Doc TPH = TPH.Doc
+instance Printer TPL where type Doc TPL = TPL.Doc
+instance Printer TPF where type Doc TPF = TPF.Doc ()
+instance Printer TPAL where type Doc TPAL = TPAL.Doc
+instance Printer TPM where type Doc TPM = TPM.Doc
 
 -- I'm pretty sure this could be generified, TH-ified etc.
--- (Excuse the longwinded code, I did not have time to write short one)
 
 data Op = Hcat | Hsep | Vcat | Sep | Cat | Fsep | Fcat
   deriving (Eq, Ord, Show, Generic)
 
-instance Pr Op where pr = text . map toLower . show
+instance Pr Op where pr = Doc.text . map toLower . show
 
 instance Serial Op
+ where series = Series $ [Cat, Sep] : repeat []
 
 data Term = Leaf
        | Branch Op [Term]
@@ -45,7 +61,7 @@ class Eval d where
   size :: d -> Int
 
 instance Eval TPH.Doc where
-  size = length . render
+  size = length . TPH.render
   eval t = case t of
     Leaf -> TPH.text "l"
     Branch op ts ->
@@ -68,14 +84,30 @@ instance Eval (TPF.Doc ()) where
       let fun = case op of Hcat -> TPF.hcat ; Hsep -> TPF.hsep ; Vcat -> TPF.vcat ; Sep -> TPF.sep ; Cat -> TPF.cat ; Fsep -> TPF.fillSep ; Fcat -> TPF.fillCat
       in  TPF.align $ fun $ map eval ts
 
-class Pr a where pr :: a -> Doc
+instance Eval TPAL.Doc where
+  size d = length $ TPAL.displayS ( TPAL.renderPretty 0.4 80 d ) ""
+  eval t = case t of
+    Leaf -> TPAL.text "l"
+    Branch op ts ->
+      let fun = case op of Hcat -> TPAL.hcat ; Hsep -> TPAL.hsep ; Vcat -> TPAL.vcat ; Sep -> TPAL.sep ; Cat -> TPAL.cat ; Fsep -> TPAL.fillSep ; Fcat -> TPAL.fillCat
+      in  TPAL.align $ fun $ map eval ts
+
+instance Eval TPM.Doc where
+  size d = length $ TPM.displayS ( TPM.render 80 d ) ""
+  eval t = case t of
+    Leaf -> TPM.text "l"
+    Branch op ts ->
+      let fun = case op of Hcat -> TPM.stack ; Hsep -> TPM.spread ; Vcat -> TPM.stack ; Sep -> TPM.sep ; Cat -> TPM.cat ; Fsep -> TPM.spread ; Fcat -> TPM.stack
+      in  TPM.align $ fun $ map eval ts
+
+class Pr a where pr :: a -> Doc.Doc
 
 instance Pr Term where
   pr t = case t of
-    Leaf -> text "text " <+> doubleQuotes (text "l")
-    Branch op ts -> pr op <+> brackets (hsep $ punctuate comma $ map pr ts)
+    Leaf -> Doc.text "text " Doc.<+> Doc.doubleQuotes (Doc.text "l")
+    Branch op ts -> pr op Doc.<+> Doc.brackets (Doc.hsep $ Doc.punctuate Doc.comma $ map pr ts)
 
-instance Show Term where show = render . pr
+instance Show Term where show = Doc.render . pr
 
 instance Serial Term
 
@@ -87,11 +119,11 @@ instance Serial Context
 
 instance Pr Context where
   pr c = case c of
-    Hole -> text "hole"
+    Hole -> Doc.text "hole"
     CBranch op pre c post ->
-      pr op <+> brackets (hsep $ punctuate comma $ map pr pre ++ [pr c] ++ map pr post)
+      pr op Doc.<+> Doc.brackets (Doc.hsep $ Doc.punctuate Doc.comma $ map pr pre ++ [pr c] ++ map pr post)
 
-instance Show Context where show = render . pr
+instance Show Context where show = Doc.render . pr
 
 apply :: Context -> Term -> Term
 apply c t = case c of
@@ -107,34 +139,41 @@ time_whnf x = do
   return $ diffUTCTime end start
 
 -- | first argument: number of iterations of this context
-check_context :: Eval d => Proxy d -> Int -> Context -> Term -> IO NominalDiffTime
-check_context p n ctx b = do
+-- check_context :: Eval (Doc d) => (d :: Tag) -> Int -> Context -> Term -> IO NominalDiffTime
+check_context (_ :: Proxy d) n ctx b = do
   let d = iterate (apply ctx) b !! n
-  time_whnf ( size ( asProxyTypeOf (eval d) p ) )
+  time_whnf ( size ( (eval d) :: Doc d  ) )
 
 -- | just count how many textdetails were emitted
-norender :: Doc -> Int
-norender = fullRender PageMode 100 1.5 ( \ _ c -> succ c ) 0 
+norender :: Doc.Doc -> Int
+norender = Doc.fullRender Doc.PageMode 100 1.5 ( \ _ c -> succ c ) 0 
 
 
 data Case = Case { c :: Context, b :: Term, n :: Int, t :: NominalDiffTime }
 
 instance Pr Case where
   pr (Case c b n t) =
-    text "length $ render $ iterate ( \\ hole -> " <+> pr c <+> text ")" <+> parens (pr b) <+> text "!!" <+> int n
-    <+> text " -- " <+> text (show t)
+    Doc.text "length $ render $ iterate ( \\ hole -> "
+    Doc.<+> pr c
+    Doc.<+> Doc.text ")"
+    Doc.<+> Doc.parens (pr b)
+    Doc.<+> Doc.text "!!"
+    Doc.<+> Doc.int n
+    Doc.<+> Doc.text "-- "
+    Doc.<+> Doc.text (show t)
 
-instance Show Case where show = render . pr
+instance Show Case where show = Doc.render . pr
 
 -- | first argument: no. of contexts to generate,
 -- second: max. iterations for each,
 -- returns worst offender (with max total sum)
-check_contexts :: Eval d => Proxy d -> Int -> IO Case
-check_contexts p it = do
+-- check_contexts :: Eval (Doc d) => Doc d -> Int -> IO Case
+check_contexts (p :: Proxy d) it = do
   let go c [] = return c
       go c (ctx : ctxs) = do
         out <- check_context p it ctx Leaf
         let c' = Case ctx Leaf it out
         if (t c' > t c) then do print c'; go c' ctxs else go c ctxs
   go (Case Hole Leaf 0 0) $ contents series
+
 
--- test/Run.hs
+++ test/Run.hs
@@ -1,9 +1,9 @@
+{-# language DataKinds #-}
+{-# language TypeApplications #-}
+
 import Lib
-import Data.Proxy
 import System.Timeout
-import qualified Text.PrettyPrint.HughesPJ as TPH 
-import qualified Text.PrettyPrint.Leijen as TPL
-import qualified Text.PrettyPrint.Free as TPF
+
 import System.Environment ( getArgs )
 import Control.Monad ( void)
 
@@ -14,7 +14,11 @@ main :: IO ()
 main = do
   argv <- getArgs
   case argv of
-      [ ] -> void $ timeout (10 * 10^6) $ check_contexts (Proxy :: Proxy TPH.Doc) 1000
-      [ "pretty", s ] -> void $ check_contexts (Proxy :: Proxy TPH.Doc) (read s)
-      [ "wl-pprint", s] -> void $ check_contexts (Proxy :: Proxy TPL.Doc) (read s)
-      [ "wl-pprint-extras", s] -> void $ check_contexts (Proxy :: Proxy (TPF.Doc ())) (read s)
+      [ ] -> void $ timeout (10 * 10^6) $ check_contexts (Proxy :: Proxy 'TPH) 1000
+      [ "pretty", s ] -> void $ check_contexts (Proxy :: Proxy 'TPH ) (read s)
+      [ "wl-pprint", s] -> void $ check_contexts (Proxy :: Proxy 'TPL) (read s)
+      [ "wl-pprint-extras", s] -> void $ check_contexts (Proxy :: Proxy 'TPF) (read s)
+      -- [ "wl-pprint-text", s] -> void $ check_contexts (Proxy :: Proxy 'TPF) (read s)
+      [ "ansi-wl-pprint", s] -> void $ check_contexts (Proxy :: Proxy 'TPAL) (read s)
+      [ "mainland-pretty", s] -> void $ check_contexts (Proxy :: Proxy 'TPM) (read s)
+
